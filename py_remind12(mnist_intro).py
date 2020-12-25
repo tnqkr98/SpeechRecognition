fe12ea27abbd3dc72755628b2304308b03ced269
Park Dong Han <tnqkr98@naver.com>
2020-12-25 22:13:27 +0900

    remind mnist

--- py_remind12(mnist_intro).py
+++ py_remind12(mnist_intro).py
@@ -24,3 +24,53 @@ mnist_test = dsets.MNIST(root='MNIST_data/',
                          train=False,
                          transform=transforms.ToTensor(),
                          download=True)
+
+# dataset loader
+data_loader = torch.utils.data.DataLoader(dataset=mnist_train,
+                                          batch_size=batch_size,
+                                          shuffle=True,
+                                          drop_last=True)
+
+linear = torch.nn.Linear(784,10,bias=True).to(device)
+
+criterion = torch.nn.CrossEntropyLoss().to(device)
+optimizer = torch.optim.SGD(linear.parameters(),lr=0.1)
+
+for epoch in range(training_epochs):
+    avg_cost = 0
+    total_batch = len(data_loader)
+
+    for X,Y in data_loader:
+        X = X.view(-1,28*28).to(device)
+        Y = Y.to(device)
+
+        optimizer.zero_grad()
+        h = linear(X)
+        cost = criterion(h,Y)
+        cost.backward()
+        optimizer.step()
+
+        avg_cost += cost
+
+    print('epoch : {:d} , avg_cost = {:.3f}'.format(epoch,avg_cost/total_batch))
+
+with torch.no_grad():
+    X_test = mnist_test.test_data.view(-1,28*28).float().to(device)
+    Y_test = mnist_test.test_labels.to(device)
+
+    prediction = linear(X_test)
+    correct_prediction = torch.argmax(prediction,1) == Y_test
+    accuracy = correct_prediction.float().mean()
+    print('accuracy:',accuracy.item())
+
+    # Get one and predict
+    r = random.randint(0,len(mnist_test)-1)
+    X_single_data = mnist_test.test_data[r:r+1].view(-1,28*28).float().to(device)
+    Y_single_data = mnist_test.test_labels[r:r+1].to(device)
+
+    print('Label :',Y_single_data)
+    single_prediction = linear(X_single_data)
+    print('prediction : ',torch.argmax(single_prediction,1).item())
+
+    plt.imshow(mnist_test.test_data[r:r+1].view(28,28),cmap='Greys',interpolation='nearest')
+    plt.show()
