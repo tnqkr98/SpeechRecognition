30e4e90700958f5c16a1ead69a86197a8f8bd3c0
Park Dong Han <tnqkr98@naver.com>
2021-02-12 22:40:07 +0900

    mnist mlp

--- py_remind12(mnist_mlp).py
+++ py_remind12(mnist_mlp).py
@@ -23,17 +23,18 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=BATCH
 
 # 미니배치 하나 확인  ( batch-size, channel, x, y ) -> channel : 1 이므로 그레이스케일
 for (X_train, Y_train) in train_loader:
-    print('X_train : ', X_train.size(), 'type:', X_train.type())
-    print('Y_train : ', Y_train.size(), 'type:', Y_train.type())
-
-    pltsize = 1
-    plt.figure(figsize=(10 * pltsize, pltsize))
-    for i in range(10):
-        plt.subplot(1, 10, i + 1)
-        plt.axis('off')
-        plt.imshow(X_train[i, :, :, :].numpy().reshape(28, 28), cmap="gray_r")
-        plt.title('Class: ' + str(Y_train[i].item()))
+    print('x_train : ', X_train.size(), 'type:', X_train.type())
+    print('y_train : ', Y_train.size(), 'type:', Y_train.type())
     break
+
+pltsize = 1
+plt.figure(figsize=(10 * pltsize, pltsize))
+for i in range(10):
+    plt.subplot(1, 10, i + 1)
+    plt.axis('off')
+    plt.imshow(X_train[i, :, :, :].numpy().reshape(28, 28), cmap="gray_r")
+    plt.title('Class: ' + str(Y_train[i].item()))
+
 
 class Net(nn.Module):
     def __init__(self):
@@ -42,7 +43,7 @@ class Net(nn.Module):
         self.fc2 = nn.Linear(512, 256)
         self.fc3 = nn.Linear(256, 10)
 
-    def forword(self, x):
+    def forward(self, x):
         x = x.view(-1,28*28)    # 2차원 -> 1차원 (Flatten 펼친다)
         x = self.fc1(x)
         x = F.sigmoid(x)
@@ -52,12 +53,6 @@ class Net(nn.Module):
         x = F.log_softmax(x, dim=1)     # gradient 원활하게 계산
         return x
 
-def train(model, train_loader, optimizer, log_interval):
-    model.train()
-    for batch_idx, (image, label) in enumerate(train_loader):
-
-
-
 
 model = Net().to(DEVICE)
 optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
@@ -66,7 +61,47 @@ criterion = nn.CrossEntropyLoss()       # output ( one-hot e
 print(model)
 
 
+def train(model, train_loader, optimizer, log_interval):
+    model.train()       # 모델을 '학습'상태로 지정
+    for batch_idx, (image, label) in enumerate(train_loader):
+        image = image.to(DEVICE)
+        label = label.to(DEVICE)
+        optimizer.zero_grad()
+        output = model(image)
+        loss = criterion(output, label)
+        loss.backward()
+        optimizer.step()
 
+        if batch_idx & log_interval == 0:
+            print("Train Epoch : {} [{}/{}({:.0f}%)]\tTrain Loss: {:.6f}".format(
+                Epoch, batch_idx*len(image), len(train_loader.dataset),
+                100.*batch_idx/len(train_loader), loss.item()
+            ))
+
+
+def evaluate(model, test_loader):
+    model.eval()    # 모델을 '평가 상태'로 지정
+    test_loss = 0
+    correct = 0
+
+    with torch.no_grad():
+        for image, label in test_loader:
+            image = image.to(DEVICE)
+            label = label.to(DEVICE)
+            output = model(image)
+            test_loss += criterion(output, label).item()
+            prediction = output.max(1, keepdim=True)[1]
+            correct += prediction.eq(label.view_as(prediction)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    test_accuracy = 100.*correct / len(test_loader.dataset)
+    return test_loss, test_accuracy
+
+
+for Epoch in range(1, EPOCHS +1):
+    train(model, train_loader, optimizer, log_interval=200)
+    test_loss, test_accuracy = evaluate(model, test_loader)
+    print("\n[EPOCH: {}], \tTest Loss: {:.4f}, \tTest Accuracy: {:.2f} % \n".format(Epoch, test_loss, test_accuracy))
 
 
 
